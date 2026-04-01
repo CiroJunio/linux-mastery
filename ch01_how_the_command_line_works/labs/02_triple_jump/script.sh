87301675a7c3d58f9e306a5a46dd9c5090e649d2
Ciro Junio <cirojunioteixeira1@gmail.com>
2026-04-01 12:57:35 -0300

    feat(02_triple_jump): Create a file using an absolute path.

--- ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
+++ ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
@@ -1,8 +1,10 @@
 # For error
+#!/bin/bash
 set -e
 
 #1. Create a dir in home
-dir="projeto/data/raw/logs"
-mkdir -p "$dir"
+PROJECT_DIR="$HOME/projeto/data/raw/logs"
+mkdir -p "$PROJECT_DIR"
 
 #2. Create a archive using absolute way
+touch "$PROJECT_DIR/setup.log"
