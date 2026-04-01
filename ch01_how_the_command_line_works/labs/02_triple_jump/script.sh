fdae970a6ae1846e129587eceedfb49486c67239
Ciro Junio <cirojunioteixeira1@gmail.com>
2026-04-01 12:47:18 -0300

    fix(02_triple_jump): Fix the mkdir line

--- ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
+++ ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
@@ -1,7 +1,8 @@
 # For error
 set -e
 
-#1. Create
-dir="projeto/data/raw"
-mkdir -p "$logs"
+#1. Create a dir in home
+dir="projeto/data/raw/logs"
+mkdir -p "$dir"
 
+#2. Create a archive using absolute way
