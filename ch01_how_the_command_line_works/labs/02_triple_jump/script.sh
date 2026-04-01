3adc7a0d80b32378f2f9697b9108ca095c3c9a45
Ciro Junio <cirojunioteixeira1@gmail.com>
2026-04-01 13:05:09 -0300

    feat(02_triple_jump): Show relative path

--- ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
+++ ch01_how_the_command_line_works/labs/02_triple_jump/script.sh
@@ -3,8 +3,14 @@
 set -e
 
 #1. Create a dir in home
-PROJECT_DIR="$HOME/projeto/data/raw/logs"
+BASE_DIR="$HOME/projeto"
+PROJECT_DIR="$BASE_DIR/data/raw/logs"
 mkdir -p "$PROJECT_DIR"
 
 #2. Create a archive using absolute way
 touch "$PROJECT_DIR/setup.log"
+
+#3. Show relative path
+cd "$BASE_DIR"
+ls -lh data/raw/logs/setup.log
+
